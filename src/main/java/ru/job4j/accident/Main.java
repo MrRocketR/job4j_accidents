1457bbef3beeb4b19cb9073aa2318bdee22eabb7
MrRocketR <kuptsovns@gmail.com>
2022-12-21 12:40:46 +0200

    0. Spring MVC

---
+++ src/main/java/ru/job4j/accident/Main.java
@@ -0,0 +1,14 @@
+package ru.job4j.accident;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8080/index");
+    }
+}
