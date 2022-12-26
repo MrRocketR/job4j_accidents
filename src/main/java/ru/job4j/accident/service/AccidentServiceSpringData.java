1550b4974c6879f72c20b2fb3ad7d9c04abe4d44
MrRocketR <kuptsovns@gmail.com>
2022-12-26 21:24:06 +0200

    Form с композиционным объектом

--- src/main/java/ru/job4j/accident/service/AccidentServiceSpringData.java
+++ src/main/java/ru/job4j/accident/service/AccidentServiceSpringData.java
@@ -3,11 +3,7 @@ package ru.job4j.accident.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.job4j.accident.model.Accident;
-import ru.job4j.accident.repository.AccidentRepository;
 import ru.job4j.accident.repository.AccidentRepositorySpringData;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @AllArgsConstructor
