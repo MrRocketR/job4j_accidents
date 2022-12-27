5a87fb98b78d7c46588de6631c1436e92606a8b3
MrRocketR <kuptsovns@gmail.com>
2022-12-27 21:27:23 +0200

    0. Spring DataSource

--- src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/accident/repository/AccidentJdbcTemplate.java
@@ -1,29 +1,88 @@
 package ru.job4j.accident.repository;
 
-import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.model.AccidentType;
+import ru.job4j.accident.model.Rule;
 
-import java.util.List;
+import java.util.*;
 
 @Repository
-@AllArgsConstructor
 public class AccidentJdbcTemplate {
     private final JdbcTemplate jdbc;
+    private Map<Integer, Rule> rules;
+    private Map<Integer, AccidentType> types;
 
-    public Accident save(Accident accident) {
-        jdbc.update("insert into accident (name) values (?)",
-                accident.getName());
+    public AccidentJdbcTemplate(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+        rules = getRulesForDB();
+        types = getTypesForDB();
+    }
+
+    public Accident save(Accident accident, String[] ids) {
+        String updateSQL = "insert into accident (name, text, address, type_id) values (?,?,?,?)";
+        String updateManyToMany = "insert into rules_accidents (fk_accident_id, fk_rules_id) values (?,?)";
+        jdbc.update(updateSQL, accident.getName(),
+                accident.getText(),
+                accident.getAddress(),
+                accident.getType().getId());
+
+        for (String id : ids) {
+            jdbc.update(updateManyToMany, accident.getId(), Integer.valueOf(id));
+        }
         return accident;
     }
 
+    public Map<Integer, Rule> getRulesForDB() {
+        String sql = "select from rules";
+        List<Rule> list = jdbc.query(
+                sql,
+                (rs, rowNum) ->
+                        new Rule(
+                                rs.getInt("id"),
+                                rs.getString("name")
+                        )
+        );
+        for (Rule r : list) {
+            rules.put(r.getId(), r);
+        }
+        return rules;
+    }
+
+    public Collection<Rule> getRulesList() {
+        return rules.values();
+    }
+
+    public Map<Integer, AccidentType> getTypesForDB() {
+        String sql = "select from accident_type";
+        List<AccidentType> list = jdbc.query(
+                sql,
+                (rs, rowNum) ->
+                        new AccidentType(
+                                rs.getInt("id"),
+                                rs.getString("name")
+                        )
+        );
+        for (AccidentType ac : list) {
+            types.put(ac.getId(), ac);
+        }
+        return types;
+    }
+
+    public Collection<AccidentType> getTypesList() {
+        return types.values();
+    }
+
     public List<Accident> getAll() {
-        return jdbc.query("select id, name from accident",
+        String sql = "select id, name, text, address, type_id from accidents";
+        return jdbc.query(sql,
                 (rs, row) -> {
                     Accident accident = new Accident();
                     accident.setId(rs.getInt("id"));
                     accident.setName(rs.getString("name"));
+                    accident.setAddress(rs.getString("address"));
+                    accident.setType(types.get(rs.getInt("type_id")));
                     return accident;
                 });
     }
--- src/main/java/ru/job4j/accident/service/AccidentServiceJDBC.java
+++ src/main/java/ru/job4j/accident/service/AccidentServiceJDBC.java
@@ -1,16 +1,34 @@
 package ru.job4j.accident.service;
 
 import lombok.AllArgsConstructor;
+import org.apache.tomcat.util.digester.Rules;
 import org.springframework.stereotype.Service;
 import ru.job4j.accident.model.Accident;
+import ru.job4j.accident.model.AccidentType;
+import ru.job4j.accident.model.Rule;
 import ru.job4j.accident.repository.AccidentJdbcTemplate;
+
+import java.util.Collection;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class AccidentServiceJDBC {
     private final AccidentJdbcTemplate accidentsRepostiory;
 
-    public void create(Accident accident) {
-        accidentsRepostiory.save(accident);
+    public void create(Accident accident, String[] ids) {
+        accidentsRepostiory.save(accident, ids);
+    }
+
+    public Collection<Accident> showALL() {
+        return accidentsRepostiory.getAll();
+    }
+
+    public Collection<Rule> getRules() {
+        return accidentsRepostiory.getRulesList();
+    }
+
+    public Collection<AccidentType> getTypes() {
+        return accidentsRepostiory.getTypesList();
     }
 }
