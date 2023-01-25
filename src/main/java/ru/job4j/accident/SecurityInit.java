98b75c2a4e824e3b33d93859808245be92f03195
MrRocketR <kuptsovns@gmail.com>
2023-01-25 13:26:13 +0200

    0. Spring Security

---
+++ src/main/java/ru/job4j/accident/SecurityInit.java
@@ -0,0 +1,10 @@
+package ru.job4j.accident;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+    
+}
