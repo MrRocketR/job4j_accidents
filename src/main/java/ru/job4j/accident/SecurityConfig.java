c0b0cfbaa8793d044dc35f2f2eba91f0cfa256c7
MrRocketR <kuptsovns@gmail.com>
2023-01-25 13:32:23 +0200

    0. Spring Security

--- src/main/java/ru/job4j/accident/SecurityConfig.java
+++ src/main/java/ru/job4j/accident/SecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.job4j.accident;
 
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
