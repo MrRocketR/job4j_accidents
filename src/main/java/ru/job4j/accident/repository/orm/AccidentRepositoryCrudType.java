221d2d39785081647c1e92641add625156854a75
MrRocketR <kuptsovns@gmail.com>
2023-01-31 19:20:05 +0200

    0. Spring test

--- src/main/java/ru/job4j/accident/repository/orm/AccidentRepositoryCrudType.java
+++ src/main/java/ru/job4j/accident/repository/orm/AccidentRepositoryCrudType.java
@@ -3,10 +3,11 @@ package ru.job4j.accident.repository.orm;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.AccidentType;
 
 import java.util.List;
-
+@Repository
 public interface AccidentRepositoryCrudType extends CrudRepository<AccidentType, Integer> {
     @Query("from AccidentType")
     List<AccidentType> getTypes();
--- src/main/java/ru/job4j/accident/repository/orm/UserRepository.java
+++ src/main/java/ru/job4j/accident/repository/orm/UserRepository.java
@@ -1,8 +1,9 @@
 package ru.job4j.accident.repository.orm;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.User;
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
