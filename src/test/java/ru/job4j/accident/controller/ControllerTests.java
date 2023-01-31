3cc73660557a57b504dc3be3ebb3c4d22c93a429
MrRocketR <kuptsovns@gmail.com>
2023-01-31 18:53:44 +0200

    0. Spring test

--- src/test/java/ru/job4j/accident/controller/ControllerTests.java
+++ src/test/java/ru/job4j/accident/controller/ControllerTests.java
@@ -1,3 +1,5 @@
+package ru.job4j.accident.controller;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
